ca27744f1b777941b305d4900e4494b64f96016d
dudabrum <eduardadebrum94@gmail.com>
2018-11-25 11:49:23 -0200

    Criação da estrutura do projeto.

---
+++ src/main/java/com/sicredidigitalpautas/eduardabrum/SicrediDigitalPautaApplication.java
@@ -0,0 +1,15 @@
+package com.sicredidigitalpautas.eduardabrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe para inicialização do Deploy da aplicação.
+ */
+@SpringBootApplication
+public class SicrediDigitalPautaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SicrediDigitalPautaApplication.class, args);
+    }
+}
